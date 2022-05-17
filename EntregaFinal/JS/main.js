44822d9bd0b753b40c937ab1dec7c1baaf603493
Sofia Cortes <soficortes@live.com.ar>
2022-05-16 22:34:12 -0300

    se crea proyecto final

--- EntregaFinal/JS/main.js
+++ EntregaFinal/JS/main.js
@@ -1,48 +1,49 @@
 let jugadores = [];
 
-localStorage.getItem("jugador") != null ? jugadores = JSON.parse(localStorage.getItem("jugador")) : jugadores = [];
+( async () => {
+
+const { value: email } = await Swal.fire({
+  title: 'Input email address',
+  input: 'email',
+  inputLabel: 'Your email address',
+  inputPlaceholder: 'Enter your email address'
+})
+
+if (email) {
+  Swal.fire(`Entered email: ${email}`)
+  jugadores.push(email)
+  localStorage.setItem('jugador', JSON.stringify(jugadores));
+  abrirNotificacion();
+}
+})()
+
+localStorage.getItem('jugador') != null
+  ? (jugadores = JSON.parse(localStorage.getItem('jugador')))
+  : (jugadores = []);
 
 class Jugador {
-    constructor (name, apellido) {
-        this.nombre = name;
-        this.apellido = apellido;
-        this.partidasJugadas = 0;
-        this.partidasGanadas = 0;
-        this.partidasPerdidas = 0;
-    }
+  constructor(email) {
+    this.mail = email;
+    this.partidasJugadas = 0;
+    this.partidasGanadas = 0;
+    this.partidasPerdidas = 0;
+  }
 }
 
 // Los parametros de partidasJugadas, partidasGanadas y partidasPerdidas van a ser incluídas mas adelante del curso.
 
-let inputNombre = document.getElementById("nombre");
-let inputApellido = document.getElementById("apellido");
-let btn = document.getElementById("boton");
-
-btn.addEventListener("click", obtenerDatos);
-
-function obtenerDatos(e) {
-    e.preventDefault();
-    let nombre = inputNombre.value;
-    console.log(nombre);
-    let apellido = inputApellido.value;
-    console.log(apellido);
-    jugadores.push(new Jugador(nombre, apellido)); 
-    localStorage.setItem("jugador", JSON.stringify(jugadores));
-    abrirNotificacion()
-}
-
 function abrirNotificacion() {
-    Toastify({
-        text: "El jugador fue cargado con éxito",
-        duration: 3000,
-        offset: {
-            x: 50,
-            y: 10
-        },
-        style: {
-          background: "linear-gradient(to right, #00b09b, #96c93d)",
-        }
-      }).showToast();
+  Toastify({
+    text: 'El jugador fue cargado con éxito',
+    duration: 3000,
+    offset: {
+      x: 100,
+      y: 10,
+    },
+    style: {
+      background: 'linear-gradient(to right, #00b09b, #96c93d)',
+    },
+  }).showToast();
 }
 
 console.log(jugadores);
@@ -51,57 +52,86 @@ fetch('https://api.pexels.com/v1/photos/4226910%27')
   .then((resp) => resp.json())
   .then((data) => {
     console.log(data);
-    const div = document.getElementById('app1');
-    div.innerHTML += `<img src=${data.src.small}>`;
-    body.appendChild(div);
+    const btnTijera = document.getElementById('btntijera');
+    let imagenTijera= `<img src=${data.src.small}>`;
+    btnTijera.innerHTML += imagenTijera;
   });
 
-// let entrada = prompt("Elige piedra, papel o tijera:")
-
-// function numAleatorio() {
-//     return Math.floor(Math.random() * 3 + 1)
-// }
+fetch('https://api.pexels.com/v1/photos/2363901')
+  .then((resp) => resp.json())
+  .then((data) => {
+    console.log(data);
+    const btnPiedra = document.getElementById('btnpiedra');
+    let imagenPiedra= `<img src=${data.src.small}>`;
+    btnPiedra.innerHTML += imagenPiedra;
+  });
   
-// const aleatorioGenerado = numAleatorio()
-// let partida
+fetch('https://api.pexels.com/v1/photos/8823492')
+  .then((resp) => resp.json())
+  .then((data) => {
+    console.log(data);
+    const btnPapel = document.getElementById('btnpapel');
+    let imagenPapel= `<img src=${data.src.small}>`;
+    btnPapel.innerHTML += imagenPapel;
+  });
+  
+  function numAleatorio() {
+    return Math.floor(Math.random() * 3 + 1)
+  }
+  
+const aleatorioGenerado = numAleatorio()
 
-// do {
-//     switch (entrada) {
-//         case "piedra":
-//             if (aleatorioGenerado == 1) {
-//                 alert("Empataste! La computadora eligió: " + aleatorioGenerado);
-//             } else if (aleatorioGenerado == 2) {
-//                 alert("Perdiste! La computadora eligió: " + aleatorioGenerado);
-//             } else {
-//                 alert("Ganaste! La computadora eligió: " + aleatorioGenerado);
-//             }
-//             break;
-//         case "papel":
-//             if (aleatorioGenerado == 1) {
-//                 alert("Ganaste! La computadora eligió: " + aleatorioGenerado);
-//             } else if (aleatorioGenerado == 2) {
-//                 alert("Empataste! La computadora eligió: " + aleatorioGenerado);
-//             } else {
-//                 alert("Perdiste! La computadora eligió: " + aleatorioGenerado);
-//             }
-//             break;
-//         case "tijera":
-//             if (aleatorioGenerado == 1) {
-//                 alert("Perdiste! La computadora eligió: " + aleatorioGenerado);
-//             } else if (aleatorioGenerado == 2) {
-//                 alert("Ganaste! La computadora eligió: " + aleatorioGenerado);
-//             } else {
-//                 alert("Empataste! La computadora eligió: " + aleatorioGenerado);
-//             }
-//             break;
-//         default: 
-//             alert("Debes ingresar piedra, papel o tijera.")
-//             break;
-//     }
-//     partida = confirm("¿Desea seguir jugando?")
-//     if (partida) {
-//         entrada = prompt("Elige piedra, papel o tijera:")
-//     } 
-// } while (partida)
+let partida
+  
+const btnpiedra = document.getElementById('btnpiedra');
 
-// alert("¡Gracias por jugar!")
+const btnpapel = document.getElementById('btnpapel');
+
+const btntijera = document.getElementById('btntijera');
+
+const seleccionJugador =  document.getElementById('app1');
+
+const seleccionPrograma = document.getElementById('app2');
+
+function jugadorEscogio () {
+  seleccionJugador.innerText = "PIEDRA"
+}
+
+function programaEscogio (){
+  seleccionPrograma.innerText = aleatorioGenerado
+}
+
+btnpiedra.addEventListener('click', () => {
+  contenedor2.classList.toggle('contenedor2');
+  jugadorEscogio();
+  programaEscogio();
+});
+
+
+
+btnpapel.addEventListener('click', () => {
+  if (aleatorioGenerado == 1) {
+    alert("Ganaste! La computadora eligió: " + aleatorioGenerado);
+} else if (aleatorioGenerado == 2) {
+    alert("Empataste! La computadora eligió: " + aleatorioGenerado);
+} else {
+    alert("Perdiste! La computadora eligió: " + aleatorioGenerado);
+}
+});
+
+btntijera.addEventListener('click', () => {
+  if (aleatorioGenerado == 1) {
+    alert("Perdiste! La computadora eligió: " + aleatorioGenerado);
+  } else if (aleatorioGenerado == 2) {
+    alert("Ganaste! La computadora eligió: " + aleatorioGenerado);
+  } else {
+    alert("Empataste! La computadora eligió: " + aleatorioGenerado);
+  }
+});
+
+
+// partida = confirm("¿Desea se`guir jugando?")
+//   if (partida) {
+//     entrada = prompt("Elige piedra, papel o tijera:")
+//   } while (partida)
+//     alert("¡Gracias por jugar!")
